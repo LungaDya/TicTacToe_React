97050db9db75e8e2b8a648d1f6efe87a9a96755e
Lunga <lungadyantyi12@gmail.com>
2025-01-28 15:37:15 +0200

    deplyment bug, and added playlist

---
+++ src/components/utils/MusicUtils/playlist.js
@@ -0,0 +1,18 @@
+const playList = [
+    "https://jetsetradio.live/radio/stations/lofi/90sFlav - Balmy.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/90sFlav - Call Me.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Adam Kay - Rehab (Instrumental).mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Aekasora - almost there.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Afternoon - spring (w park bird).mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Altitude - lake serene.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/App - My First.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Aso - Coffee.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/athena - one side.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Bknapp - Game Over (Super Mario Lofi).mp3",
+    "https://jetsetradio.live/radio/stations/lofi/blvknight - december.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Burbank - A Beauitful Name.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Burbank - Moonlights.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/Burbank - The Shadow Of Your Smile.mp3",
+    "https://jetsetradio.live/radio/stations/lofi/cat naps - hazy dreams.mp3",
+
+]
